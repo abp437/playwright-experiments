d57cf37e1f3034fb1e0250fa2dc1fd76823fb11f
abp437 <abp437@gmail.com>
2026-05-05 18:02:11 +0530

    Update title test

--- tests/title.spec.ts
+++ tests/title.spec.ts
@@ -3,24 +3,5 @@ import { BASE_URL } from "../constants";
 
 test("Attendance App title", async ({ page }) => {
   await page.goto(BASE_URL);
-
-  // Check page title
   await expect(page).toHaveTitle("IDS Attendance Tracker");
-
-  // Fill email
-  await page.getByRole("textbox", { name: "Email" }).fill("abp437@gmail.com");
-
-  // Fill password
-  await page.getByRole("textbox", { name: "Password" }).fill("SecurePass!333!");
-
-  // Click login
-  await page.getByRole("button", { name: "Login" }).click();
-
-  // Wait for navigation to /dashboard
-  await page.waitForURL("**/dashboard"); // matches any protocol + host with /dashboard
-
-  // Optionally, assert URL is correct
-  await expect(page).toHaveURL(/.*\/dashboard/);
-
-  await page.pause();
 });
