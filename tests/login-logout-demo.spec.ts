9c99fd7e466345642cfe986ba626afe99216771b
abp437 <abp437@gmail.com>
2026-05-05 17:58:53 +0530

    Add tests specific to IDS Attendance App

--- tests/login-logout-demo.spec.ts
+++ tests/login-logout-demo.spec.ts
@@ -4,54 +4,45 @@ import { BASE_URL } from "../constants";
 // Helper function for filling out the login form
 async function login(page: Page, email: string, password: string) {
   await page.goto(`${BASE_URL}/login`);
-  await page.locator("#emailAddress").fill(email);
-  await page.locator("#password").fill(password);
-  await page.locator('button[type="submit"]').click();
+  await page.getByRole("textbox", { name: "Email" }).fill(email);
+  await page.getByRole("textbox", { name: "Password" }).fill(password);
+  await page.getByRole("button", { name: "Login" }).click();
 }
 
 // Helper function to verify that a user is logged out
 async function logout(page: Page) {
-  await page.locator("button#radix-_r_4_").click();
-  await page.getByRole("menuitem", { name: "Log out" }).click();
-  await expect(page).toHaveURL(`${BASE_URL}/login`);
+  await page.locator('[data-test-selector="user-navbar-popover"]').click();
+  await page.getByRole("button", { name: "Logout" }).click();
+  await expect(page).toHaveURL(`${BASE_URL}/congrats`);
 }
 
-// Test for non-existent user login
 test("Non-existent user login", async ({ page }) => {
   await login(page, "dafsdf@gmail.com", "sdfdsafjhjv!Sd!1!");
-  await expect(page.getByText("Email or password not recognized.")).toBeVisible();
+  await expect(page.getByText("Login failed")).toBeVisible();
 });
 
-// Test for invalid credentials login
 test("Invalid credentials login", async ({ page }) => {
-  await login(page, "jambhaleprajkta@gmail.com", "SecurePass!1!");
-  await expect(
-    page.getByText(
-      "An unexpected problem on our end prevented you from logging in. Please refresh the page and try again.",
-    ),
-  ).toBeVisible();
+  await login(page, "abp437@gmail.com", "SecurePass!1!");
+  await expect(page.getByText("Login failed")).toBeVisible();
 });
 
-// Test for Super Admin login
-test("Super admin login", async ({ page }) => {
-  await login(page, "akshaypawar@doodle-blue.com", "SecurePass!1!");
-  await expect(page).toHaveURL(`${BASE_URL}/analytics`, { timeout: 30000 });
-  await expect(page.getByText("Product Metrics")).toBeVisible();
-  await logout(page); // Use the helper function for logging out
-});
-
-// Test for Admin login
 test("Admin login", async ({ page }) => {
-  await login(page, "abp9834@gmail.com", "SecurePass!1!");
-  await expect(page).toHaveURL(`${BASE_URL}/dashboard`, { timeout: 30000 });
-  await expect(page.getByText("Today's Sites")).toBeVisible();
+  await login(page, "abp437@gmail.com", "SecurePass!333!");
+  await expect(page).toHaveURL(`${BASE_URL}/dashboard`, { timeout: 3000 });
+  await expect(page.getByText("Today's All Teams Summary")).toBeVisible();
   await logout(page);
 });
 
-// Test for Support professional login
-test("Support professional login", async ({ page }) => {
-  await login(page, "jambhaleprajkta@gmail.com", "SecurePass!1!");
-  await expect(page).toHaveURL(`${BASE_URL}/dashboard`, { timeout: 30000 });
-  await expect(page.getByText("Today's Sites")).toBeVisible();
+test("Manager login", async ({ page }) => {
+  await login(page, "akashshelar@gmail.com", "SecurePass!333!");
+  await expect(page).toHaveURL(`${BASE_URL}/my-team`, { timeout: 3000 });
+  await expect(page.getByText("Today's Team Summary")).toBeVisible();
+  await logout(page);
+});
+
+test("Employee login", async ({ page }) => {
+  await login(page, "drprajktajambhale99@gmail.com", "Emp@333!");
+  await expect(page).toHaveURL(`${BASE_URL}/calendar`, { timeout: 3000 });
+  await expect(page.getByText("Current Leave Stats")).toBeVisible();
   await logout(page);
 });
